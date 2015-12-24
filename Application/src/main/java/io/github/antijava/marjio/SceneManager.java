b278b44b49e8fb3b3dafefcea2aa6e68484f3a32
David Kuo <s50407s@gmail.com>
2015-12-24 21:17:30 +0800

    Add simple scene manager
    Without translation animations.

---
+++ Application/src/main/java/io/github/antijava/marjio/SceneManager.java
@@ -0,0 +1,65 @@
+package io.github.antijava.marjio;
+
+import io.github.antijava.marjio.common.IApplication;
+import io.github.antijava.marjio.common.IScene;
+import io.github.antijava.marjio.common.ISceneManager;
+
+/**
+ * Manage the current scene and provides translation.
+ */
+public class SceneManager implements ISceneManager {
+    private final IApplication mApplication;
+    private IScene mScene = null;
+    private IScene mLastScene = null;
+    private boolean mTranslating = false;
+
+    public SceneManager(IApplication application) {
+        mApplication = application;
+    }
+
+    @Override
+    public void update() {
+        if (translate())
+            return;
+
+        if (mScene == null)
+            return;
+
+        mScene.update();
+    }
+
+    @Override
+    public void translationTo(IScene scene) {
+        if (mScene == scene)
+            return;
+
+        mLastScene = mScene;
+        mScene = scene;
+
+        prepareTranslation();
+    }
+
+    // region Translating
+
+    /**
+     * Scene translating.
+     * @return if break update chain, that is, not to update the scenes.
+     */
+    private boolean translate() {
+        if (!mTranslating)
+            return false;
+
+        // TODO: Add translating animation
+
+        // Finish translation
+        mTranslating = false;
+        mScene = mLastScene;
+        return false;
+    }
+
+    private void prepareTranslation() {
+        mTranslating = true;
+        // TODO: Add translating animation
+    }
+    // endregion Translating
+}
