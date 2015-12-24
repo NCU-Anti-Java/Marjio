7e6d14fa04ee67b7a9e0f053a39018fecd013977
David Kuo <s50407s@gmail.com>
2015-12-24 22:39:55 +0800

    Create Main and build artifical

--- Common/src/main/java/io/github/antijava/marjio/common/IApplication.java
+++ Common/src/main/java/io/github/antijava/marjio/common/IApplication.java
@@ -1,9 +1,12 @@
 package io.github.antijava.marjio.common;
 
+import java.util.logging.Logger;
+
 /**
  * Created by fntsr on 2015/12/23.
  */
 public interface IApplication {
+    Logger getLogger();
     ISceneManager getSceneManager();
     IInput getInput();
     IServer getServer();
