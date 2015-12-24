fbbddfdd21e143a17362a827b433154b8f7223d6
firejox <firejox@gmail.com>
2015-12-24 22:58:59 +0800

    Define Keys

---
+++ Common/src/main/java/io/github/antijava/marjio/common/Key.java
@@ -0,0 +1,1160 @@
+package io.github.antijava.marjio.common;
+
+/**
+ * Created by freyr on 2015/12/24.
+ */
+
+
+public enum Key {
+
+    JUMP,
+
+    MOVE_LEFT,
+
+    MOVE_RIGHT,
+
+    CROUCH,
+
+
+
+
+
+    /*************************************
+     *                                   *
+     *           General Key             *
+     *                                   *
+     * **********************************/
+
+    /**
+     * Constant for the {@code Enter} key.
+     */
+    ENTER,
+
+    /**
+     * Constant for the {@code Backspace} key.
+     */
+    BACK_SPACE,
+
+    /**
+     * Constant for the {@code Tab} key.
+     */
+    TAB,
+
+    /**
+     * Constant for the {@code Cancel} key.
+     */
+    CANCEL,
+
+    /**
+     * Constant for the {@code Clear} key.
+     */
+    CLEAR,
+
+    /**
+     * Constant for the {@code Shift} key.
+     */
+    SHIFT,
+
+    /**
+     * Constant for the {@code Ctrl} key.
+     */
+    CONTROL,
+
+    /**
+     * Constant for the {@code Alt} key.
+     */
+    ALT,
+
+    /**
+     * Constant for the {@code Pause} key.
+     */
+    PAUSE,
+
+    /**
+     * Constant for the {@code Caps Lock} key.
+     */
+    CAPS,
+
+    /**
+     * Constant for the {@code Esc} key.
+     */
+    ESCAPE,
+
+    /**
+     * Constant for the {@code Space} key.
+     */
+    SPACE,
+
+    /**
+     * Constant for the {@code Page Up} key.
+     */
+    PAGE_UP,
+
+    /**
+     * Constant for the {@code Page Down} key.
+     */
+    PAGE_DOWN,
+
+    /**
+     * Constant for the {@code End} key.
+     */
+    END,
+
+    /**
+     * Constant for the {@code Home} key.
+     */
+    HOME,
+
+    /**
+     * Constant for the non-numpad <b>left</b> arrow key.
+     */
+    LEFT,
+
+    /**
+     * Constant for the non-numpad <b>up</b> arrow key.
+     */
+    UP,
+
+    /**
+     * Constant for the non-numpad <b>right</b> arrow key.
+     */
+    RIGHT,
+
+    /**
+     * Constant for the non-numpad <b>down</b> arrow key.
+     */
+    DOWN,
+
+    /**
+     * Constant for the comma key, ","
+     */
+    COMMA,
+
+    /**
+     * Constant for the minus key, "-"
+     */
+    MINUS,
+
+    /**
+     * Constant for the period key, "."
+     */
+    PERIOD,
+
+    /**
+     * Constant for the forward slash key, "/"
+     */
+    SLASH,
+
+    /**
+     * Constant for the {@code 0} key.
+     */
+    DIGIT0,
+
+    /**
+     * Constant for the {@code 1} key.
+     */
+    DIGIT1,
+
+    /**
+     * Constant for the {@code 2} key.
+     */
+    DIGIT2,
+
+    /**
+     * Constant for the {@code 3} key.
+     */
+    DIGIT3,
+
+    /**
+     * Constant for the {@code 4} key.
+     */
+    DIGIT4,
+
+    /**
+     * Constant for the {@code 5} key.
+     */
+    DIGIT5,
+
+    /**
+     * Constant for the {@code 6} key.
+     */
+    DIGIT6,
+
+    /**
+     * Constant for the {@code 7} key.
+     */
+    DIGIT7,
+
+    /**
+     * Constant for the {@code 8} key.
+     */
+    DIGIT8,
+
+    /**
+     * Constant for the {@code 9} key.
+     */
+    DIGIT9,
+
+    /**
+     * Constant for the semicolon key, ";"
+     */
+    SEMICOLON,
+
+    /**
+     * Constant for the equals key, "="
+     */
+    EQUALS,
+
+    /**
+     * Constant for the {@code A} key.
+     */
+    A,
+
+    /**
+     * Constant for the {@code B} key.
+     */
+    B,
+
+    /**
+     * Constant for the {@code C} key.
+     */
+    C,
+
+    /**
+     * Constant for the {@code D} key.
+     */
+    D,
+
+    /**
+     * Constant for the {@code E} key.
+     */
+    E,
+
+    /**
+     * Constant for the {@code F} key.
+     */
+    F,
+
+    /**
+     * Constant for the {@code G} key.
+     */
+    G,
+
+    /**
+     * Constant for the {@code H} key.
+     */
+    H,
+
+    /**
+     * Constant for the {@code I} key.
+     */
+    I,
+
+    /**
+     * Constant for the {@code J} key.
+     */
+    J,
+
+    /**
+     * Constant for the {@code K} key.
+     */
+    K,
+
+    /**
+     * Constant for the {@code L} key.
+     */
+    L,
+
+    /**
+     * Constant for the {@code M} key.
+     */
+    M,
+
+    /**
+     * Constant for the {@code N} key.
+     */
+    N,
+
+    /**
+     * Constant for the {@code O} key.
+     */
+    O,
+
+    /**
+     * Constant for the {@code P} key.
+     */
+    P,
+
+    /**
+     * Constant for the {@code Q} key.
+     */
+    Q,
+
+    /**
+     * Constant for the {@code R} key.
+     */
+    R,
+
+    /**
+     * Constant for the {@code S} key.
+     */
+    S,
+
+    /**
+     * Constant for the {@code T} key.
+     */
+    T,
+
+    /**
+     * Constant for the {@code U} key.
+     */
+    U,
+
+    /**
+     * Constant for the {@code V} key.
+     */
+    V,
+
+    /**
+     * Constant for the {@code W} key.
+     */
+    W,
+
+    /**
+     * Constant for the {@code X} key.
+     */
+    X,
+
+    /**
+     * Constant for the {@code Y} key.
+     */
+    Y,
+
+    /**
+     * Constant for the {@code Z} key.
+     */
+    Z,
+
+    /**
+     * Constant for the open bracket key, "["
+     */
+    OPEN_BRACKET,
+
+    /**
+     * Constant for the back slash key, "\"
+     */
+    BACK_SLASH,
+
+    /**
+     * Constant for the close bracket key, "]"
+     */
+    CLOSE_BRACKET,
+
+    /**
+     * Constant for the {@code Numpad 0} key.
+     */
+    NUMPAD0,
+
+    /**
+     * Constant for the {@code Numpad 1} key.
+     */
+    NUMPAD1,
+
+    /**
+     * Constant for the {@code Numpad 2} key.
+     */
+    NUMPAD2,
+
+    /**
+     * Constant for the {@code Numpad 3} key.
+     */
+    NUMPAD3,
+
+    /**
+     * Constant for the {@code Numpad 4} key.
+     */
+    NUMPAD4,
+
+    /**
+     * Constant for the {@code Numpad 5} key.
+     */
+    NUMPAD5,
+
+    /**
+     * Constant for the {@code Numpad 6} key.
+     */
+    NUMPAD6,
+
+    /**
+     * Constant for the {@code Numpad 7} key.
+     */
+    NUMPAD7,
+
+    /**
+     * Constant for the {@code Numpad 8} key.
+     */
+    NUMPAD8,
+
+    /**
+     * Constant for the {@code Numpad 9} key.
+     */
+    NUMPAD9,
+
+    /**
+     * Constant for the {@code Multiply} key.
+     */
+    MULTIPLY,
+
+    /**
+     * Constant for the {@code Add} key.
+     */
+    ADD,
+
+    /**
+     * Constant for the Numpad Separator key.
+     */
+    SEPARATOR,
+
+    /**
+     * Constant for the {@code Subtract} key.
+     */
+    SUBTRACT,
+
+    /**
+     * Constant for the {@code Decimal} key.
+     */
+    DECIMAL,
+
+    /**
+     * Constant for the {@code Divide} key.
+     */
+    DIVIDE,
+
+    /**
+     * Constant for the {@code Delete} key.
+     */
+    DELETE, /* ASCII:Integer   DEL */
+
+    /**
+     * Constant for the {@code Num Lock} key.
+     */
+    NUM_LOCK,
+
+    /**
+     * Constant for the {@code Scroll Lock} key.
+     */
+    SCROLL_LOCK,
+
+    /**
+     * Constant for the F1 function key.
+     */
+    F1,
+
+    /**
+     * Constant for the F2 function key.
+     */
+    F2,
+
+    /**
+     * Constant for the F3 function key.
+     */
+    F3,
+
+    /**
+     * Constant for the F4 function key.
+     */
+    F4,
+
+    /**
+     * Constant for the F5 function key.
+     */
+    F5,
+
+    /**
+     * Constant for the F6 function key.
+     */
+    F6,
+
+    /**
+     * Constant for the F7 function key.
+     */
+    F7,
+
+    /**
+     * Constant for the F8 function key.
+     */
+    F8,
+
+    /**
+     * Constant for the F9 function key.
+     */
+    F9,
+
+    /**
+     * Constant for the F10 function key.
+     */
+    F10,
+
+    /**
+     * Constant for the F11 function key.
+     */
+    F11,
+
+    /**
+     * Constant for the F12 function key.
+     */
+    F12,
+
+    /**
+     * Constant for the F13 function key.
+     */
+    F13,
+
+    /**
+     * Constant for the F14 function key.
+     */
+    F14,
+
+    /**
+     * Constant for the F15 function key.
+     */
+    F15,
+
+    /**
+     * Constant for the F16 function key.
+     */
+    F16,
+
+    /**
+     * Constant for the F17 function key.
+     */
+    F17,
+
+    /**
+     * Constant for the F18 function key.
+     */
+    F18,
+
+    /**
+     * Constant for the F19 function key.
+     */
+    F19,
+
+    /**
+     * Constant for the F20 function key.
+     */
+    F20,
+
+    /**
+     * Constant for the F21 function key.
+     */
+    F21,
+
+    /**
+     * Constant for the F22 function key.
+     */
+    F22,
+
+    /**
+     * Constant for the F23 function key.
+     */
+    F23,
+
+    /**
+     * Constant for the F24 function key.
+     */
+    F24,
+
+    /**
+     * Constant for the {@code Print Screen} key.
+     */
+    PRINTSCREEN,
+
+    /**
+     * Constant for the {@code Insert} key.
+     */
+    INSERT,
+
+    /**
+     * Constant for the {@code Help} key.
+     */
+    HELP,
+
+    /**
+     * Constant for the {@code Meta} key.
+     */
+    META,
+
+    /**
+     * Constant for the {@code Back Quote} key.
+     */
+    BACK_QUOTE,
+
+    /**
+     * Constant for the {@code Quote} key.
+     */
+    QUOTE,
+
+    /**
+     * Constant for the numeric keypad <b>up</b> arrow key.
+     */
+    KP_UP,
+
+    /**
+     * Constant for the numeric keypad <b>down</b> arrow key.
+     */
+    KP_DOWN,
+
+    /**
+     * Constant for the numeric keypad <b>left</b> arrow key.
+     */
+    KP_LEFT,
+
+    /**
+     * Constant for the numeric keypad <b>right</b> arrow key.
+     */
+    KP_RIGHT,
+
+    /**
+     * Constant for the {@code Dead Grave} key.
+     */
+    DEAD_GRAVE,
+
+    /**
+     * Constant for the {@code Dead Acute} key.
+     */
+    DEAD_ACUTE,
+
+    /**
+     * Constant for the {@code Dead Circumflex} key.
+     */
+    DEAD_CIRCUMFLEX,
+
+    /**
+     * Constant for the {@code Dead Tilde} key.
+     */
+    DEAD_TILDE,
+
+    /**
+     * Constant for the {@code Dead Macron} key.
+     */
+    DEAD_MACRON,
+
+    /**
+     * Constant for the {@code Dead Breve} key.
+     */
+    DEAD_BREVE,
+
+    /**
+     * Constant for the {@code Dead Abovedot} key.
+     */
+    DEAD_ABOVEDOT,
+
+    /**
+     * Constant for the {@code Dead Diaeresis} key.
+     */
+    DEAD_DIAERESIS,
+
+    /**
+     * Constant for the {@code Dead Abovering} key.
+     */
+    DEAD_ABOVERING,
+
+    /**
+     * Constant for the {@code Dead Doubleacute} key.
+     */
+    DEAD_DOUBLEACUTE,
+
+    /**
+     * Constant for the {@code Dead Caron} key.
+     */
+    DEAD_CARON,
+
+    /**
+     * Constant for the {@code Dead Cedilla} key.
+     */
+    DEAD_CEDILLA,
+
+    /**
+     * Constant for the {@code Dead Ogonek} key.
+     */
+    DEAD_OGONEK,
+
+    /**
+     * Constant for the {@code Dead Iota} key.
+     */
+    DEAD_IOTA,
+
+    /**
+     * Constant for the {@code Dead Voiced Sound} key.
+     */
+    DEAD_VOICED_SOUND,
+
+    /**
+     * Constant for the {@code Dead Semivoiced Sound} key.
+     */
+    DEAD_SEMIVOICED_SOUND,
+
+    /**
+     * Constant for the {@code Ampersand} key.
+     */
+    AMPERSAND,
+
+    /**
+     * Constant for the {@code Asterisk} key.
+     */
+    ASTERISK,
+
+    /**
+     * Constant for the {@code Double Quote} key.
+     */
+    QUOTEDBL,
+
+    /**
+     * Constant for the {@code Less} key.
+     */
+    LESS,
+
+    /**
+     * Constant for the {@code Greater} key.
+     */
+    GREATER,
+
+    /**
+     * Constant for the {@code Left Brace} key.
+     */
+    BRACELEFT,
+
+    /**
+     * Constant for the {@code Right Brace} key.
+     */
+    BRACERIGHT,
+
+    /**
+     * Constant for the "@" key.
+     */
+    AT,
+
+    /**
+     * Constant for the ":" key.
+     */
+    COLON,
+
+    /**
+     * Constant for the "^" key.
+     */
+    CIRCUMFLEX,
+
+    /**
+     * Constant for the "$" key.
+     */
+    DOLLAR,
+
+    /**
+     * Constant for the Euro currency sign key.
+     */
+    EURO_SIGN,
+
+    /**
+     * Constant for the "!" key.
+     */
+    EXCLAMATION_MARK,
+
+    /**
+     * Constant for the inverted exclamation mark key.
+     */
+    INVERTED_EXCLAMATION_MARK,
+
+    /**
+     * Constant for the "(" key.
+     */
+    LEFT_PARENTHESIS,
+
+    /**
+     * Constant for the "#" key.
+     */
+    NUMBER_SIGN,
+
+    /**
+     * Constant for the "+" key.
+     */
+    PLUS,
+
+    /**
+     * Constant for the ")" key.
+     */
+    RIGHT_PARENTHESIS,
+
+    /**
+     * Constant for the "_" key.
+     */
+    UNDERSCORE,
+
+    /**
+     * Constant for the Microsoft Windows "Windows" key.
+     * It is used for both the left and right version of the key.
+     */
+    WINDOWS,
+
+    /**
+     * Constant for the Microsoft Windows Context Menu key.
+     */
+    CONTEXT_MENU,
+
+    /**
+     * Constant for input method support on Asian Keyboards.
+     */
+    FINAL,
+
+    /**
+     * Constant for the Convert function key.
+     */
+    CONVERT,
+
+    /**
+     * Constant for the Don't Convert function key.
+     */
+    NONCONVERT,
+
+    /**
+     * Constant for the Accept or Commit function key.
+     */
+    ACCEPT,
+
+    /**
+     * Constant for the {@code Mode Change} key.
+     */
+    MODECHANGE,
+    /**
+     * Constant for the {@code Kana} key.
+     */
+    KANA,
+    /**
+     * Constant for the {@code Kanji} key.
+     */
+    KANJI,
+
+    /**
+     * Constant for the Alphanumeric function key.
+     */
+    ALPHANUMERIC,
+
+    /**
+     * Constant for the Katakana function key.
+     */
+    KATAKANA,
+
+    /**
+     * Constant for the Hiragana function key.
+     */
+    HIRAGANA,
+
+    /**
+     * Constant for the Full-Width Characters function key.
+     */
+    FULL_WIDTH,
+
+    /**
+     * Constant for the Half-Width Characters function key.
+     */
+    HALF_WIDTH,
+
+    /**
+     * Constant for the Roman Characters function key.
+     */
+    ROMAN_CHARACTERS,
+
+    /**
+     * Constant for the All Candidates function key.
+     */
+    ALL_CANDIDATES,
+
+    /**
+     * Constant for the Previous Candidate function key.
+     */
+    PREVIOUS_CANDIDATE,
+
+    /**
+     * Constant for the Code Input function key.
+     */
+    CODE_INPUT,
+
+    /**
+     * Constant for the Japanese-Katakana function key.
+     * This key switches to a Japanese input method and selects its Katakana input mode.
+     */
+    JAPANESE_KATAKANA,
+
+    /**
+     * Constant for the Japanese-Hiragana function key.
+     * This key switches to a Japanese input method and selects its Hiragana input mode.
+     */
+    JAPANESE_HIRAGANA,
+
+    /**
+     * Constant for the Japanese-Roman function key.
+     * This key switches to a Japanese input method and selects its Roman-Direct input mode.
+     */
+    JAPANESE_ROMAN,
+
+    /**
+     * Constant for the locking Kana function key.
+     * This key locks the keyboard into a Kana layout.
+     */
+    KANA_LOCK,
+
+    /**
+     * Constant for the input method on/off key.
+     */
+    INPUT_METHOD_ON_OFF,
+
+    /**
+     * Constant for the {@code Cut} key.
+     */
+    CUT,
+
+    /**
+     * Constant for the {@code Copy} key.
+     */
+    COPY,
+
+    /**
+     * Constant for the {@code Paste} key.
+     */
+    PASTE,
+
+    /**
+     * Constant for the {@code Undo} key.
+     */
+    UNDO,
+
+    /**
+     * Constant for the {@code Again} key.
+     */
+    AGAIN,
+
+    /**
+     * Constant for the {@code Find} key.
+     */
+    FIND,
+
+    /**
+     * Constant for the {@code Properties} key.
+     */
+    PROPS,
+
+    /**
+     * Constant for the {@code Stop} key.
+     */
+    STOP,
+
+    /**
+     * Constant for the input method on/off key.
+     */
+    COMPOSE,
+
+    /**
+     * Constant for the AltGraph function key.
+     */
+    ALT_GRAPH,
+
+    /**
+     * Constant for the Begin key.
+     */
+    BEGIN,
+
+    /**
+     * This value is used to indicate that the keyCode is unknown.
+     * Key typed events do not have a keyCode value; this value
+     * is used instead.
+     */
+    UNDEFINED,
+
+
+    //--------------------------------------------------------------
+    //
+    // Mobile and Embedded Specific Key Codes
+    //
+    //--------------------------------------------------------------
+
+    /**
+     * Constant for the {@code Softkey 0} key.
+     */
+    SOFTKEY_0,
+
+    /**
+     * Constant for the {@code Softkey 1} key.
+     */
+    SOFTKEY_1,
+
+    /**
+     * Constant for the {@code Softkey 2} key.
+     */
+    SOFTKEY_2,
+
+    /**
+     * Constant for the {@code Softkey 3} key.
+     */
+    SOFTKEY_3,
+
+    /**
+     * Constant for the {@code Softkey 4} key.
+     */
+    SOFTKEY_4,
+
+    /**
+     * Constant for the {@code Softkey 5} key.
+     */
+    SOFTKEY_5,
+
+    /**
+     * Constant for the {@code Softkey 6} key.
+     */
+    SOFTKEY_6,
+
+    /**
+     * Constant for the {@code Softkey 7} key.
+     */
+    SOFTKEY_7,
+
+    /**
+     * Constant for the {@code Softkey 8} key.
+     */
+    SOFTKEY_8,
+
+    /**
+     * Constant for the {@code Softkey 9} key.
+     */
+    SOFTKEY_9,
+
+    /**
+     * Constant for the {@code Game A} key.
+     */
+    GAME_A,
+
+    /**
+     * Constant for the {@code Game B} key.
+     */
+    GAME_B,
+
+    /**
+     * Constant for the {@code Game C} key.
+     */
+    GAME_C,
+
+    /**
+     * Constant for the {@code Game D} key.
+     */
+    GAME_D,
+
+    /**
+     * Constant for the {@code Star} key.
+     */
+    STAR,
+
+    /**
+     * Constant for the {@code Pound} key.
+     */
+    POUND,
+
+    /**
+     * Constant for the {@code Power} key.
+     */
+    POWER,
+
+    /**
+     * Constant for the {@code Info} key.
+     */
+    INFO,
+
+    /**
+     * Constant for the {@code Colored Key 0} key.
+     */
+    COLORED_KEY_0,
+
+    /**
+     * Constant for the {@code Colored Key 1} key.
+     */
+    COLORED_KEY_1,
+
+    /**
+     * Constant for the {@code Colored Key 2} key.
+     */
+    COLORED_KEY_2,
+
+    /**
+     * Constant for the {@code Colored Key 3} key.
+     */
+    COLORED_KEY_3,
+
+    /**
+     * Constant for the {@code Eject} key.
+     */
+    EJECT_TOGGLE,
+
+    /**
+     * Constant for the {@code Play} key.
+     */
+    PLAY,
+
+    /**
+     * Constant for the {@code Record} key.
+     */
+    RECORD,
+
+    /**
+     * Constant for the {@code Fast Forward} key.
+     */
+    FAST_FWD,
+
+    /**
+     * Constant for the {@code Rewind} key.
+     */
+    REWIND,
+
+    /**
+     * Constant for the {@code Previous Track} key.
+     */
+    TRACK_PREV,
+
+    /**
+     * Constant for the {@code Next Track} key.
+     */
+    TRACK_NEXT,
+
+    /**
+     * Constant for the {@code Channel Up} key.
+     */
+    CHANNEL_UP,
+
+    /**
+     * Constant for the {@code Channel Down} key.
+     */
+    CHANNEL_DOWN,
+
+    /**
+     * Constant for the {@code Volume Up} key.
+     */
+    VOLUME_UP,
+
+    /**
+     * Constant for the {@code Volume Down} key.
+     */
+    VOLUME_DOWN,
+
+    /**
+     * Constant for the {@code Mute} key.
+     */
+    MUTE,
+
+    /**
+     * Constant for the Apple {@code Command} key.
+     * @since JavaFX 2.1
+     */
+    COMMAND,
+
+    /**
+     * Constant for the {@code Shortcut} key.
+     */
+    SHORTCUT
+}
