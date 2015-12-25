048de0fbb2e8ac9fcb80dcaa0a2f2feb0db5dcc7
Shark <sharknevercries@gmail.com>
2015-12-25 13:13:29 +0800

    Add join scene base

---
+++ Application/src/main/java/io/github/antijava/marjio/scene/JoinScene.java
@@ -0,0 +1,112 @@
+package io.github.antijava.marjio.scene;
+
+import io.github.antijava.marjio.common.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Zheng-Yuan on 12/24/2015.
+ */
+public class JoinScene extends SceneBase {
+    private final static List<Key> VALID_INPUT = new ArrayList<Key>(Arrays.asList(
+            Key.LEFT, Key.RIGHT, Key.UP, Key.DOWN,
+            Key.BACK_SPACE, Key.DELETE, Key.DECIMAL,
+            Key.NUMPAD0, Key.DIGIT0,
+            Key.NUMPAD1, Key.DIGIT1,
+            Key.NUMPAD2, Key.DIGIT2,
+            Key.NUMPAD3, Key.DIGIT3,
+            Key.NUMPAD4, Key.DIGIT4,
+            Key.NUMPAD5, Key.DIGIT5,
+            Key.NUMPAD6, Key.DIGIT6,
+            Key.NUMPAD7, Key.DIGIT7,
+            Key.NUMPAD8, Key.DIGIT8,
+            Key.NUMPAD9, Key.DIGIT9,
+            Key.ENTER
+    ));
+    private final String[] MENU_TEXT = {"Remote Address", "Go Back"};
+    private final int IPADDRESS_MAX_LENGTH = 15;
+    private final int INPUT_IPADDRESS = 0;
+    private final int GO_BACK = 1;
+    private int mCurrentChoice;
+    private StringBuffer mIPAddress;
+    private int mCurrentAddressIndex;
+
+    public JoinScene(IApplication application) {
+        super(application);
+    }
+
+    @Override
+    public void update() {
+        checkKeyState();
+        // TODO: Draw view. Mark current choice as other color.
+    }
+
+    @Override
+    public void initialize() {
+        mCurrentChoice = 0;
+        mCurrentAddressIndex = 0;
+        mIPAddress = new StringBuffer();
+    }
+
+    private void checkKeyState() {
+        final IInput input = getApplication().getInput();
+
+        for (Iterator it = VALID_INPUT.iterator(); it.hasNext(); ) {
+            Key key = (Key)it.next();
+            // TODO: Add key
+            if (input.isPressed() || input.isPressing()) {
+                switch (key) {
+                    case Key.UP: {
+                        if (--mCurrentChoice < 0)
+                            mCurrentChoice = 0;
+                        break;
+                    }
+                    case Key.DOWN: {
+                        if (++mCurrentChoice >= MENU_TEXT.length)
+                            mCurrentChoice = MENU_TEXT.length - 1;
+                        break;
+                    }
+                    case Key.ENTER: {
+                        select();
+                        break;
+                    }
+                    default: {
+                        // TODO: Do we need man-made input box? Deal with valid input such as 0..9 , '.', backspace, delete.
+                        if (mCurrentChoice == INPUT_IPADDRESS)
+                            ;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    private void select() {
+        switch (mCurrentChoice) {
+            case INPUT_IPADDRESS: {
+                if(connect()) {
+                    // TODO: Transate to room scene with client state.
+                }
+                else {
+                    // TODO: Show Error.
+                }
+                break;
+            }
+            case GO_BACK: {
+                final ISceneManager sceneManager = getApplication().getSceneManager();
+                sceneManager.translationTo(new MainScene(getApplication()));
+                break;
+            }
+        }
+    }
+
+    private boolean connect() {
+        final IClient client = getApplication().getClient();
+        // TODO: Connect to server
+        return true;
+    }
+}
