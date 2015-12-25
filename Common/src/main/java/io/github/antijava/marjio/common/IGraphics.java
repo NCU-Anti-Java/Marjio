11ffb7da8c7b7b9540e63b5be13631f85205c6a0
David Kuo <s50407s@gmail.com>
2015-12-25 14:49:56 +0800

    Add Bitmap interface
    Add some basic utils.

--- Common/src/main/java/io/github/antijava/marjio/common/IGraphics.java
+++ Common/src/main/java/io/github/antijava/marjio/common/IGraphics.java
@@ -1,7 +1,10 @@
 package io.github.antijava.marjio.common;
 
+import io.github.antijava.marjio.common.graphics.IFont;
+
 /**
  * Created by fntsr on 2015/12/23.
  */
 public interface IGraphics {
+    IFont getDefaultFont();
 }
