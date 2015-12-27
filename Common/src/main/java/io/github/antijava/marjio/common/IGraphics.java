7afa1c5c8443ac622d8fb7476730f543c245f95f
David Kuo <s50407s@gmail.com>
2015-12-27 16:16:51 +0800

    Create bitmap form Graphics.

--- Common/src/main/java/io/github/antijava/marjio/common/IGraphics.java
+++ Common/src/main/java/io/github/antijava/marjio/common/IGraphics.java
@@ -1,10 +1,32 @@
 package io.github.antijava.marjio.common;
 
+import io.github.antijava.marjio.common.graphics.IBitmap;
 import io.github.antijava.marjio.common.graphics.IFont;
+
+import java.nio.file.NoSuchFileException;
 
 /**
  * Created by fntsr on 2015/12/23.
  */
 public interface IGraphics {
     IFont getDefaultFont();
+
+    /**
+     * Allocate a bitmap of declared size.
+     *
+     * @param width The width of the bitmap.
+     * @param height The height of the bitmap.
+     *
+     * @return The bitmap. If failed, returns {@code null}.
+     */
+    IBitmap createBitmap(int width, int height);
+
+    /**
+     * Load bitmap from file.
+     *
+     * @param path The path of the file should be loaded.
+     *
+     * @return The bitmap.
+     */
+    IBitmap loadBitmap(String path) throws NoSuchFileException;
 }
