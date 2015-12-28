32e2382645e736f38caa0a99e1d7e7ac76f61908
YWJamesLin <JamesLin@ssinrc.org>
2015-12-28 23:04:13 +0800

    fix IClient issue

--- Common/src/main/java/io/github/antijava/marjio/common/IClient.java
+++ Common/src/main/java/io/github/antijava/marjio/common/IClient.java
@@ -1,11 +1,43 @@
 package io.github.antijava.marjio.common;
 
+import io.github.antijava.marjio.common.input.Status;
+
 import java.net.InetAddress;
 
 /**
  * Created by fntsr on 2015/12/23.
  */
 public interface IClient {
-    void start(InetAddress hostAddress);
-    void stop();
+    /**
+     * Start connecting to a Server
+     *
+     * @param hostAddress Server address
+     */
+    void start(InetAddress hostAddress) throws Exception;
+
+    /**
+     * Stop the connection.
+     */
+    void stop() throws Exception;
+
+    /**
+     * Send message to Server.
+     *
+     * @param status information package
+     */
+    void send(Status status) throws Exception;
+
+    /**
+     * Specific if this client is trying to connect or is connected.
+     *
+     * @return if client is running
+     */
+    boolean isRunning();
+
+    /**
+     * Specific if the connecting is connected.
+     *
+     * @return if client connected to server
+     */
+    boolean isConnected();
 }
