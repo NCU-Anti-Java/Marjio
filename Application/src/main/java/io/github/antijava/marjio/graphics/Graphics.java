7f8d1e59b9fb097d21734657a614301abf24e0eb
David Kuo <s50407s@gmail.com>
2015-12-28 17:48:31 +0800

    WindowBase Cursor

--- Application/src/main/java/io/github/antijava/marjio/graphics/Graphics.java
+++ Application/src/main/java/io/github/antijava/marjio/graphics/Graphics.java
@@ -1,22 +1,25 @@
 package io.github.antijava.marjio.graphics;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 
 import io.github.antijava.marjio.common.IApplication;
 import io.github.antijava.marjio.common.IGraphics;
 import io.github.antijava.marjio.common.graphics.IBitmap;
 import io.github.antijava.marjio.common.graphics.IFont;
 import io.github.antijava.marjio.common.graphics.ISprite;
+import io.github.antijava.marjio.common.graphics.Rectangle;
 import io.github.antijava.marjio.common.graphics.Viewport;
 import io.github.antijava.marjio.window.WindowBase;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.NoSuchFileException;
+import java.util.ArrayList;
 
 /**
  * @author Davy
@@ -61,6 +64,7 @@ public class Graphics implements IGraphics {
     public void touch() {
         try {
             windowBase = new WindowBase(mApplication, loadBitmap("windowskin/default.png"), 200, 200);
+            windowBase.setActive(true);
         } catch (NoSuchFileException e) {
             windowBase = null;
             System.exit(1);
@@ -78,6 +82,7 @@ public class Graphics implements IGraphics {
 
         windowBase.setWidth((windowBase.getWidth() + 1) % 200 + 200);
         windowBase.setHeight((windowBase.getHeight() + 3) % 200 + 200);
+        windowBase.setCursorRect(new Rectangle(0, 0, 100, 150));
         windowBase.update();
         final java.awt.Graphics g = bufferedImage.getGraphics();
         g.setColor(Color.black);
