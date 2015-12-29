aec6ce10698606fbff37755247a100045343816f
David Kuo <s50407s@gmail.com>
2015-12-29 22:23:06 +0800

    Make Join Scene more pretty

--- Application/src/main/java/io/github/antijava/marjio/scene/JoinScene.java
+++ Application/src/main/java/io/github/antijava/marjio/scene/JoinScene.java
@@ -1,69 +1,138 @@
 package io.github.antijava.marjio.scene;
 
-import io.github.antijava.marjio.common.*;
+import io.github.antijava.marjio.common.IApplication;
+import io.github.antijava.marjio.common.IInput;
+import io.github.antijava.marjio.common.ISceneManager;
+import io.github.antijava.marjio.common.graphics.Color;
+import io.github.antijava.marjio.common.graphics.IBitmap;
 import io.github.antijava.marjio.common.input.Key;
+import io.github.antijava.marjio.constant.Constant;
+import io.github.antijava.marjio.window.WindowBase;
+import io.github.antijava.marjio.window.WindowCommand;
 import io.github.antijava.marjio.window.WindowIPAddressInput;
 
 /**
  * Created by Zheng-Yuan on 12/24/2015.
  */
-public class JoinScene extends SceneBase {
-    private final String[] MENU_TEXT = {"Remote Address", "Go Back"};
-    private final int INPUT_IPADDRESS = 0;
-    private final int GO_BACK = 1;
-    private int mCurrentChoice;
+public class JoinScene extends SceneBase implements Constant {
+    private WindowBase mWindowBack;
     private WindowIPAddressInput mWindowIPAddressInput;
+    private WindowCommand mWindowCommand;
 
     public JoinScene(IApplication application) {
         super(application);
-        mCurrentChoice = 0;
-        mWindowIPAddressInput = new WindowIPAddressInput(application);
-        mWindowIPAddressInput.setActive(true);
+
+        initWindows();
+        moveWindows((GAME_WIDTH - mWindowBack.getWidth()) / 2,
+                (GAME_HEIGHT - mWindowBack.getHeight()) / 2);
     }
 
     @Override
     public void update() {
         super.update();
 
-        mWindowIPAddressInput.update();
-        checkKeyState();
-        // TODO: Draw view. Mark current choice as other color.
-    }
-
-    private void checkKeyState() {
         final IInput input = getApplication().getInput();
 
-        if (input.isPressed(Key.UP) || input.isPressing(Key.UP)) {
-            if (--mCurrentChoice < 0)
-                mCurrentChoice = 0;
+        mWindowBack.update();
+        if (input.isPressed(Key.ENTER)) {
+            if (mWindowCommand.isActive()) {
+                if (processOption())
+                    return;
+            }
         }
-        else if (input.isPressed(Key.DOWN) || input.isPressing(Key.DOWN)) {
-            if (++mCurrentChoice >= MENU_TEXT.length)
-                mCurrentChoice = MENU_TEXT.length - 1;
+        if (input.isRepeat(Key.LEFT)) {
+            if (mWindowIPAddressInput.isActive() && mWindowIPAddressInput.getIndex() == 0) {
+                mWindowIPAddressInput.setActive(false);
+                mWindowCommand.setActive(true);
+            } else if (mWindowCommand.isActive()) {
+                mWindowCommand.setActive(false);
+                mWindowIPAddressInput.setIndex(14);
+                mWindowIPAddressInput.update();
+                mWindowIPAddressInput.setActive(true);
+                return;
+            }
         }
-        else if (input.isTrigger(Key.ENTER)) {
-            // select();
+        if (input.isRepeat(Key.RIGHT)) {
+            if (mWindowIPAddressInput.isActive() && mWindowIPAddressInput.getIndex() == 14) {
+                mWindowIPAddressInput.setActive(false);
+                mWindowCommand.setActive(true);
+            } else if (mWindowCommand.isActive()) {
+                mWindowCommand.setActive(false);
+                mWindowIPAddressInput.setIndex(0);
+                mWindowIPAddressInput.update();
+                mWindowIPAddressInput.setActive(true);
+                return;
+            }
         }
+        mWindowIPAddressInput.update();
+        mWindowCommand.update();
     }
 
-    private void select() {
-        switch (mCurrentChoice) {
-            case INPUT_IPADDRESS: {
+    @Override
+    public void dispose() {
+        super.dispose();
+
+        mWindowBack.dispose();
+        mWindowCommand.dispose();
+        mWindowIPAddressInput.dispose();
+    }
+
+    private boolean processOption() {
+        switch (mWindowCommand.getIndex()) {
+            case 0: {
                 try {
                     // TODO: Transate to room scene with client state.
                     final ISceneManager sceneManager = getApplication().getSceneManager();
                     sceneManager.translationTo(new RoomScene(getApplication(), false));
+                    return true;
                 }
                 catch (Exception ex) {
                     // TODO: Show Error.
                 }
-                break;
+                return false;
             }
-            case GO_BACK: {
+            case 1: {
                 final ISceneManager sceneManager = getApplication().getSceneManager();
-                sceneManager.translationTo(new MainScene(getApplication()));
-                break;
+                sceneManager.translationTo(new MainScene(getApplication(), 1));
+                return true;
             }
         }
+        return false;
+    }
+
+    private void initWindows() {
+        final IApplication application = getApplication();
+
+        mWindowIPAddressInput = new WindowIPAddressInput(application);
+        mWindowCommand = new WindowCommand(application, 140, new String[] {"確定", "返回"});
+        mWindowBack = new WindowBase(application,
+                mWindowIPAddressInput.getWidth() + mWindowCommand.getWidth(),
+                mWindowCommand.getHeight()) {
+            @Override
+            public void update() {
+                super.update();
+
+                final IBitmap readmeContent = getContent();
+                readmeContent.clear();
+                readmeContent.drawText("輸入伺服器 IP: ", 0, 0, -1, 24, Color.WHITE);
+            }
+        };
+
+        mWindowIPAddressInput.setBackgroundOpacity(255);
+        mWindowIPAddressInput.setZ(10);
+        mWindowCommand.setIndex(1);
+        mWindowCommand.setBackgroundOpacity(255);
+        mWindowCommand.setZ(10);
+
+        mWindowIPAddressInput.setActive(true);
+    }
+
+    private void moveWindows(final int x, final int y) {
+        mWindowBack.setX(x);
+        mWindowBack.setY(y);
+        mWindowIPAddressInput.setX(x);
+        mWindowIPAddressInput.setY(y + 24);
+        mWindowCommand.setX(mWindowIPAddressInput.getWidth() + x);
+        mWindowCommand.setY(y);
     }
 }
