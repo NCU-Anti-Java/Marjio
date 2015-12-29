ed0b09d9918f474920689901888456872a1dfda2
tjjh89017 <tjjh89017@hotmail.com>
2015-12-29 16:25:18 +0800

    Update for Packer Using ig-json-parser, need to gradle clean then gradle build to gen ig-json-parser

---
+++ Application/src/main/java/io/github/antijava/marjio/network/StatusData.java
@@ -0,0 +1,34 @@
+package io.github.antijava.marjio.network;
+
+import com.instagram.common.json.annotation.JsonField;
+import com.instagram.common.json.annotation.JsonType;
+
+/**
+ * Created by Date on 2015/12/29.
+ */
+@JsonType
+public class StatusData {
+
+    /*
+     * Player 0
+     * Block  1
+     * Item   2
+     */
+    public final int Player = 0;
+    public final int Block  = 1;
+    public final int Item   = 2;
+
+    @JsonField(fieldName = "type")
+    public int type;
+
+    @JsonField(fieldName = "id")
+    public int id;
+    @JsonField(fieldName = "action_id")
+    public int action_id;
+    @JsonField(fieldName = "timer_counter")
+    public int time_counter;
+    @JsonField(fieldName = "st_x")
+    public int st_x;
+    @JsonField(fieldName = "st_y")
+    public int st_y;
+}
---
+++ Application/src/test/java/io/github/antijava/marjio/network/PackerTest.java
@@ -0,0 +1,128 @@
+package io.github.antijava.marjio.network;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Date on 2015/12/29.
+ */
+public class PackerTest {
+
+    private StatusData player;
+    private StatusData block;
+    private StatusData item;
+
+    private final int PLAYER = 0;
+    private final int BLOCK = 1;
+    private final int ITEM = 2;
+    private final int ID = 5;
+    private final int ACTION_ID = 10;
+    private final int TIMER_COUNTER = Integer.MAX_VALUE;
+    private final int ST_X = 100;
+    private final int ST_Y = 1000;
+
+    public StatusData gen(int type){
+
+        StatusData statusData = new StatusData();
+
+        statusData.type = type;
+        statusData.id = ID;
+        statusData.action_id = ACTION_ID;
+        statusData.time_counter = TIMER_COUNTER;
+        statusData.st_x = ST_X;
+        statusData.st_y = ST_Y;
+
+        return statusData;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testPack() throws Exception {
+
+        StatusData player = gen(PLAYER);
+        StatusData block = gen(BLOCK);
+        StatusData item = gen(ITEM);
+
+        StatusData player2 = gen(PLAYER);
+        StatusData block2 = gen(BLOCK);
+        StatusData item2 = gen(ITEM);
+
+        // same data, but different instance need same result JSON
+        String playerJSON = Packer.pack(player);
+        String player2JSON = Packer.pack(player2);
+        assertEquals(playerJSON, player2JSON);
+
+        String blockJSON = Packer.pack(block);
+        String blockJSON2 = Packer.pack(block2);
+        assertEquals(blockJSON, blockJSON2);
+
+        String itemJSON = Packer.pack(item);
+        String item2JSON = Packer.pack(item2);
+        assertEquals(itemJSON, item2JSON);
+
+        // different instance & data, need different result JSON
+        assertNotEquals(playerJSON, itemJSON);
+        assertNotEquals(playerJSON, blockJSON);
+        assertNotEquals(itemJSON, blockJSON);
+
+    }
+
+    @Test
+    public void testUnpack() throws Exception {
+
+        String playerJSON
+                = "{\"timer_counter\":2147483647,\"action_id\":10,\"id\":5,\"type\":0,\"st_x\":100,\"st_y\":1000}";
+        String blockJSON
+                = "{\"timer_counter\":2147483647,\"action_id\":10,\"id\":5,\"type\":1,\"st_x\":100,\"st_y\":1000}";
+        String itemJSON
+                = "{\"timer_counter\":2147483647,\"action_id\":10,\"id\":5,\"type\":2,\"st_x\":100,\"st_y\":1000}";
+
+        StatusData player = Packer.unpack(playerJSON);
+        assertEquals(player.type, PLAYER);
+        assertEquals(player.id, ID);
+        assertEquals(player.action_id, ACTION_ID);
+        assertEquals(player.time_counter, TIMER_COUNTER);
+        assertEquals(player.st_x, ST_X);
+        assertEquals(player.st_y, ST_Y);
+
+        StatusData block = Packer.unpack(blockJSON);
+        assertEquals(block.type, BLOCK);
+        assertEquals(block.id, ID);
+        assertEquals(block.action_id, ACTION_ID);
+        assertEquals(block.time_counter, TIMER_COUNTER);
+        assertEquals(block.st_x, ST_X);
+        assertEquals(block.st_y, ST_Y);
+
+        StatusData item = Packer.unpack(itemJSON);
+        assertEquals(item.type, ITEM);
+        assertEquals(item.id, ID);
+        assertEquals(item.action_id, ACTION_ID);
+        assertEquals(item.time_counter, TIMER_COUNTER);
+        assertEquals(item.st_x, ST_X);
+        assertEquals(item.st_y, ST_Y);
+
+
+        String player2JSON
+                = "{\"type\":0,\"action_id\":10,\"timer_counter\":2147483647,\"id\":5,\"st_x\":100,\"st_y\":1000}";
+        StatusData player2 = Packer.unpack(player2JSON);
+
+        assertEquals(player.type, player2.type);
+        assertEquals(player.id, player2.id);
+        assertEquals(player.action_id, player2.action_id);
+        assertEquals(player.time_counter, player2.time_counter);
+        assertEquals(player.st_x, player2.st_x);
+        assertEquals(player.st_y, player2.st_y);
+    }
+}
