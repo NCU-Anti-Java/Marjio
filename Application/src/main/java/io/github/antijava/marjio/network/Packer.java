496dfde98554736874d1a2dfab27ec17634ef5ac
tjjh89017 <tjjh89017@hotmail.com>
2015-12-29 16:25:18 +0800

    Update for Packer Using ig-json-parser, need to gradle clean then gradle build to gen ig-json-parser

--- Application/src/main/java/io/github/antijava/marjio/network/Packer.java
+++ Application/src/main/java/io/github/antijava/marjio/network/Packer.java
@@ -1,22 +1,31 @@
 package io.github.antijava.marjio.network;
 
-import java.io.IOException;
-import com.bluelinelabs.logansquare.LoganSquare;
 import io.github.antijava.marjio.common.input.Status;
+
+import java.io.IOException;
 
 /**
  * Created by Date on 2015/12/28.
  */
 public class Packer {
 
-    // 因為這部分還沒完成，若直接設置成 static 方法會導致測試無法通過，所以先去掉 static 修飾詞
-    public byte[] pack(Status status) throws IOException {
-        return LoganSquare.serialize(status).getBytes();
+    public static String pack(StatusData statusData) throws IOException {
+
+        return StatusData__JsonHelper.serializeToJson(statusData);
     }
 
-    // 因為這部分還沒完成，若直接設置成 static 方法會導致測試無法通過，所以先去掉 static 修飾詞
-    public Status unpack(byte[] bytes) throws IOException {
+    public static StatusData unpack(String string) throws IOException {
 
-        return LoganSquare.parse(new String(bytes), Status.class);
+        return StatusData__JsonHelper.parseFromJson(string);
+    }
+
+    public byte[] pack(Status status) throws IOException {
+
+        return null;
+    }
+
+    public Status unpack2(byte[] bytes) throws IOException {
+
+        return null;
     }
 }
