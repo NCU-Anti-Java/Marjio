8e117e4f4f6413cef5938f6ab9eb05d1d465687c
YWJamesLin <JamesLin@ssinrc.org>
2015-12-31 01:42:31 +0800

    Add mResourcesManager in Application and imp. drawing block feature

--- Application/src/main/java/io/github/antijava/marjio/constant/Constant.java
+++ Application/src/main/java/io/github/antijava/marjio/constant/Constant.java
@@ -3,7 +3,7 @@ package io.github.antijava.marjio.constant;
 /**
  * Created by Davy on 2015/12/24.
  */
-public interface Constant extends GameConstant, WindowConstant, ResourcesConstant, SceneObjectConstant, NetworkConstant {
+public interface Constant extends GameConstant, WindowConstant, ResourcesConstant, SceneObjectConstant {
     String PACKAGE_NAME = "io.github.antijava.marjio";
     String LOGGER_NAME = PACKAGE_NAME;
 }
