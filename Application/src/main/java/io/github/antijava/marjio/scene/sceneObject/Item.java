8c02a3c7376a1b0bcfd361742c0de2970666d594
TEMU1086 <TEMU1086@gmail.com>
2016-01-04 05:13:02 +0800

    Implement "Item" class

--- Application/src/main/java/io/github/antijava/marjio/scene/sceneObject/Item.java
+++ Application/src/main/java/io/github/antijava/marjio/scene/sceneObject/Item.java
@@ -1,7 +1,32 @@
 package io.github.antijava.marjio.scene.sceneObject;
 
 /**
- * Created by firejox on 2015/12/28.
+ * @author Jason
  */
 public class Item {
+    private final String name;
+
+    // region Enum
+    enum ItemType {
+        Trap
+    }
+    // endregion Enum
+
+    // region Constructor
+    public Item(ItemType type) {
+        switch (type) {
+            case Trap:
+                name = "Trap";
+                break;
+            default:
+                name = "";
+        }
+    }
+    // endregion Constructor
+
+    // region Getter
+    public String getName() {
+        return name;
+    }
+    // endregion Getter
 }
