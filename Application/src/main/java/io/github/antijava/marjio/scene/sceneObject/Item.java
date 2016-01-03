7a5229e2f8433366d45663ac3c77ab96f0c35637
TEMU1086 <TEMU1086@gmail.com>
2016-01-04 07:33:48 +0800

    Reimplement "Item" and extends "SceneObjectObectBase"

--- Application/src/main/java/io/github/antijava/marjio/scene/sceneObject/Item.java
+++ Application/src/main/java/io/github/antijava/marjio/scene/sceneObject/Item.java
@@ -1,43 +1,70 @@
 package io.github.antijava.marjio.scene.sceneObject;
 
+import io.github.antijava.marjio.application.Application;
+import io.github.antijava.marjio.common.IApplication;
+import io.github.antijava.marjio.common.graphics.IBitmap;
+import io.github.antijava.marjio.common.graphics.Rectangle;
+import io.github.antijava.marjio.common.graphics.Viewport;
+import io.github.antijava.marjio.resourcemanager.ResourcesManager;
+
 /**
  * @author Jason
  */
-public class Item {
-    private final String name;
-    private final ItemType type;
+public class Item extends SceneObjectObjectBase {
+    private final String mName;
+    private final ItemType mItemType;
 
     // region Enum
     public enum ItemType {
-        None,
+        TrapTool,
         Trap
     }
     // endregion Enum
 
     // region Constructor
-    public Item() {
-        this(ItemType.None);
-    }
+    public Item(Viewport viewport, IApplication application, int x, int y, int z, ItemType type) {
+        super(viewport);
 
-    public Item(ItemType type) {
-        this.type = type;
+        // Set properties
+        setX(x);
+        setY(y);
+        setZ(z);
+        this.mItemType = type;
 
+        // Set bitmap for this item
+        ResourcesManager rm = ((Application)application).getResourcesManager();
+        IBitmap bitmap;
         switch (type) {
+            case TrapTool:
+                mName = "Trap";
+                bitmap = rm.tile("default.png", 18, 6);
+                bitmap.resize(BLOCK_SIZE, BLOCK_SIZE);
+                break;
             case Trap:
-                name = "Trap";
+                mName = "Trap";
+                bitmap = rm.tile("default.png", 15, 6);
+                bitmap.resize(BLOCK_SIZE, BLOCK_SIZE);
                 break;
             default:
-                name = "";
+                mName = "";
+                bitmap = application.getGraphics().createBitmap(BLOCK_SIZE, BLOCK_SIZE);
         }
+        setBitmap(bitmap);
     }
     // endregion Constructor
 
     // region Getter
     public String getName() {
-        return name;
+        return mName;
     }
-    public ItemType getType() {
-        return type;
+
+    public ItemType getItemType() {
+        return mItemType;
+    }
+
+    @Override
+    public Rectangle getOccupiedSpace() {
+        return new Rectangle(getX(), getY(), BLOCK_SIZE, BLOCK_SIZE);
     }
     // endregion Getter
 }
