b161d24670dd29981040aabc66c6c8cc9bff4d82
firejox <firejox@gmail.com>
2016-01-04 16:41:40 +0800

    Add Item Slot

--- Application/src/main/java/io/github/antijava/marjio/scene/StageScene.java
+++ Application/src/main/java/io/github/antijava/marjio/scene/StageScene.java
@@ -28,6 +28,7 @@ import io.github.antijava.marjio.scene.sceneObject.Player;
 import io.github.antijava.marjio.scene.sceneObject.SceneMap;
 import io.github.antijava.marjio.scene.sceneObject.SceneObjectObjectBase;
 
+import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -62,6 +63,10 @@ public class StageScene extends SceneBase implements Constant {
     final WindowBase ba;
     final Player p;
 */
+    private final Sprite mItemSlot;
+    private final Sprite mItemSlotText;
+
+    private Item mItemOwned;
 
 
 
@@ -84,6 +89,27 @@ public class StageScene extends SceneBase implements Constant {
         mIsServer = IsServer;
         mPlayers = new HashMap<>();
         mItems = new ArrayList<>();
+        //Item
+        mItemSlot = new SpriteBase(GameViewPort);
+        try {
+            mItemSlot.setBitmap(graphics.loadBitmap("slot.png"));
+
+        } catch(NoSuchFileException ex) {
+            ex.printStackTrace();
+        }
+
+        mItemSlot.setX(30);
+        mItemSlot.setY(50);
+        mItemSlot.setZ(99);
+
+        mItemSlotText = new SpriteBase(GameViewPort);
+        mItemSlotText.setBitmap(graphics.createBitmap(50, 20));
+        mItemSlotText.getBitmap().drawText("Item", 0,0, 50,20, Color.WHITE, IBitmap.TextAlign.CENTER);
+        mItemSlotText.setX(30);
+        mItemSlotText.setY(30);
+        mItemSlotText.setZ(99);
+
+        mItemOwned = null;
 
         if (mIsServer) {
             mYourPlayerID = application.getServer().getMyId();
